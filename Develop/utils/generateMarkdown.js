be82e45f11c771da420f10d4d079d2eae2274a18
sm3131 <sm2683@nau.edu>
2021-12-24 15:52:19 -0700

    created function to generate license badge based on user license selection

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'Apache2.0') {
+    return `[![License Badge](https://img.shields.io/badge/license-${license}-blue)]`
+  } else if (license === 'GPL') {
+    return `[![License Badge](https://img.shields.io/badge/license-${license}-blue)]`;
+  } else if (license === 'ISC') {
+    return `[![License Badge](https://img.shields.io/badge/license-${license}-yellow)]`;
+  } else if (license === 'BSD') {
+    return `[![License Badge](https://img.shields.io/badge/license-${license}-orange)]`;
+  } else if (license === 'MIT') {
+    return `[![License Badge](https://img.shields.io/badge/license-${license}-green)]`;
+  } else if (license === undefined || 'none') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
