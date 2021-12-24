a2eea1738d4d88d65b3bd65ec950f807c53f84d3
sm3131 <sm2683@nau.edu>
2021-12-24 15:54:16 -0700

    created function to add license link to the license badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,7 +18,21 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'Apache2.0') {
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === 'GPL') {
+    return `(https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (license === 'ISC') {
+    return `(https://opensource.org/licenses/ISC)`
+  } else if (license === 'BSD') {
+    return `(https://opensource.org/licenses/BSD-3-Clause)`
+  } else if (license === 'MIT') {
+    return `(https://opensource.org/licenses/MIT)`
+  } else if (license === undefined || 'none') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
